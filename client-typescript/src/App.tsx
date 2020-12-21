7fbdc641a78764649d8e5b85c448ca9327e0198b
Keaton Sentak <k.sentak@outlook.com>
2020-12-21 15:32:48 -0500

    add pages

--- client-typescript/src/App.tsx
+++ client-typescript/src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+// Components
+import Navbar from './components/Navbar';
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App:React.FC = () => {
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/about' component={About} />
+				<Route exact path='/portfolio' component={Portfolio} />
+				<Route exact path='/contact' component={Contact} />
+				<Route component={Error} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
---
+++ client-typescript/src/pages/Portfolio.tsx
@@ -0,0 +1,11 @@
+import Projects from '../components/Projects';
+
+const Portfolio:React.FC = () => {
+	return (
+		<div className='Portfolio'>
+			<Projects />
+		</div>
+	);
+};
+
+export default Portfolio;
