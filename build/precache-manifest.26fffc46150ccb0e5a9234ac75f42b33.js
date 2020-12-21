dd53d28084f95d25e18e61e62dca8c8b1a5bfaf6
Keaton Sentak <k.sentak@outlook.com>
2020-12-21 15:13:53 -0500

    initialize typescript app

---
+++ build/precache-manifest.26fffc46150ccb0e5a9234ac75f42b33.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddb6081be27c16f200a999c732970e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5a6b04d489cb6603e3",
+    "url": "/static/css/2.b01ee654.chunk.css"
+  },
+  {
+    "revision": "13d78f8831ed8e273813",
+    "url": "/static/css/main.1bd3d73a.chunk.css"
+  },
+  {
+    "revision": "5b5a6b04d489cb6603e3",
+    "url": "/static/js/2.2d53560a.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/static/js/2.2d53560a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d78f8831ed8e273813",
+    "url": "/static/js/main.15720722.chunk.js"
+  },
+  {
+    "revision": "2b54c58a3686319f999c",
+    "url": "/static/js/runtime-main.203aac1a.js"
+  },
+  {
+    "revision": "bfdb0cd82a8ac27eb977bad8ddd9f774",
+    "url": "/static/media/GoogleBooks.bfdb0cd8.png"
+  },
+  {
+    "revision": "faf98f2a51e3803f4fb9bd7028218a71",
+    "url": "/static/media/Keaton_Sentak_Resume.faf98f2a.pdf"
+  },
+  {
+    "revision": "2084e4b72ea5654e918b04f0e5f5b5dc",
+    "url": "/static/media/liberty-beans.2084e4b7.png"
+  },
+  {
+    "revision": "35cfd0d2ff465dbaece105b0a2c32fdf",
+    "url": "/static/media/omega.35cfd0d2.png"
+  },
+  {
+    "revision": "a04b403d49560a9e865f8183cc1aab42",
+    "url": "/static/media/profile-headshot.a04b403d.jpg"
+  },
+  {
+    "revision": "7d7e15d1c0b14d3473736cddce16a5c4",
+    "url": "/static/media/react-auth.7d7e15d1.png"
+  },
+  {
+    "revision": "ff86580799f2ec28359242f0df55a0b8",
+    "url": "/static/media/react-colors.ff865807.png"
+  },
+  {
+    "revision": "2335c3e5f6535afafc150adb3f292b1b",
+    "url": "/static/media/time_machine.2335c3e5.png"
+  }
+]);
