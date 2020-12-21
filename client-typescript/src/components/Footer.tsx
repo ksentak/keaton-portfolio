149239bf16cd20209ca9af9038f1ea502770e0fc
Keaton Sentak <k.sentak@outlook.com>
2020-12-21 15:47:32 -0500

    minor styling updates

--- client-typescript/src/components/Footer.tsx
+++ client-typescript/src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer:React.FC = () => {
 	return (
 		<div className='Footer'>
 			<div className='col-12 text-center copyright'>
-				<p>Copyright &copy; 2020 Keaton Sentak</p>
+				<p>Copyright &copy; {new Date().getFullYear()} Keaton Sentak</p>
 			</div>
 		</div>
 	);
