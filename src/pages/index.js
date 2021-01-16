ba64273a5de30dc57051bb1f75b84fa186652aee
KSentak <k.sentak@outlook.com>
2021-01-16 16:43:44 -0500

    modify home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import Banner from '../components/Banner';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
+const Home = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title='Home' />
+    <div className='Home'>
+      <Banner />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default Home;
