0e34da5769e974ace9ee8f384775c1aeeb9fff83
Keaton Sentak <ksentak01@gmail.com>
2022-02-20 14:54:54 -0500

    education section

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import EducationEntry from './educationEntry';
 
 const Education: React.FC = () => {
   return (
     <div className='Education'>
-      <div className='col-12 text-center copyright mt-5'>
-        <p>Copyright &copy; {new Date().getFullYear()} Keaton Sentak</p>
+      <div className='container my-5'>
+        {/* Row 1 */}
+        <div className='row'>
+          <div className='col-12'>
+            <h1 className='section-title text-center'>Education</h1>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className='row text-start'>
+          <EducationEntry
+            schoolName='University of Pennsylvania'
+            degree='Professional Certificate - Full-Stack Development'
+            startDate='2020'
+            endDate='2021'
+          />
+          <EducationEntry
+            schoolName='Rutgers, The State University of New Jersey'
+            degree={`Bachelor's of Science - Biology`}
+            startDate='2014'
+            endDate='2018'
+          />
+        </div>
       </div>
     </div>
   );
