61727f26b0b1d83fa4f8a26e4921055a815f0ff0
Keaton Sentak <ksentak01@gmail.com>
2022-08-12 15:32:01 -0400

    rewrite with try catch

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -1,30 +1,27 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.handler = function (event, context, callback) {
+exports.handler = async (event, context, callback) => {
   const { name, email, subject, message } = JSON.parse(event.body);
 
   const msg = {
     to: process.env.ADMIN_EMAIL,
     from: email,
     subject,
-    text: `${message} \n from ${name}`,
     html: `${message} \n from ${name}`,
   };
 
-  sgMail
-    .send(msg)
-    .then((res) => {
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({ msg: 'Email sent' }),
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      return callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({ msg: 'Failed to send email.' }),
-      });
+  try {
+    await sgMail.send(msg);
+
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ msg: 'Message sent successfully.' }),
     });
+  } catch (err) {
+    return callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({ msg: 'Failed to send email.' }),
+    });
+  }
 };
