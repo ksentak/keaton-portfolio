6d15a301fa34859b4aa7fc4e077732105b77f032
Keaton Sentak <54916859+ksentak@users.noreply.github.com>
2023-12-19 13:28:35 -0500

    add new ts particles (#49)

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,52 +1,70 @@
-import React from 'react';
-import Particles from 'react-tsparticles';
+import React, { useEffect, useMemo, useState } from 'react';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { type Container, type ISourceOptions } from '@tsparticles/engine';
+import { loadSlim } from '@tsparticles/slim';
 import Socials from './socials';
 
 const Banner: React.FC = () => {
-  const config = {
-    fpsLimit: 60,
-    particles: {
-      number: {
-        value: 75,
-        density: {
-          enable: false,
-        },
-      },
-      color: {
-        value: '#fff',
-      },
-      shape: {
-        type: 'circle',
-        stroke: {
-          width: 0,
-          color: '#000000',
-        },
-        polygon: {
-          nb_sides: 5,
-        },
-      },
-      opacity: {
-        value: 0.4,
-      },
-      size: {
-        value: 5,
-        random: true,
-        anim: {
-          speed: 4,
-          size_min: 0.3,
-        },
-      },
-      move: {
-        enable: true,
-        random: true,
-        speed: 1,
-      },
-    },
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
   };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fpsLimit: 60,
+      particles: {
+        number: {
+          value: 75,
+        },
+        color: {
+          value: '#fff',
+        },
+        shape: {
+          type: 'circle',
+          stroke: {
+            width: 0,
+            color: '#000000',
+          },
+        },
+        opacity: {
+          value: 0.4,
+        },
+        size: {
+          value: {
+            min: 2,
+            max: 4,
+          },
+          random: true,
+        },
+        move: {
+          enable: true,
+          random: true,
+          speed: 1,
+        },
+      },
+    }),
+    [],
+  );
 
   return (
     <div className='Banner'>
-      <Particles className='particles' options={config} />
+      {init && (
+        <Particles
+          className='particles'
+          particlesLoaded={particlesLoaded}
+          options={options}
+        />
+      )}
       <div className='jumbotron jumbotron-fluid'>
         <div className='banner-content'>
           <h1>
