150b5b02904a7469705879658f9253f68b41bf0e
Keaton Sentak <ksentak01@gmail.com>
2023-12-22 11:21:32 -0500

    create astro site

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Keaton Sentak';
+export const SITE_DESCRIPTION = 'Personal portfolio site for Keaton Sentak';
